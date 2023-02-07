caea6c4819b83339724e58c23d0c8ac2fb196066
eduardo neves <eduardoneves858@gmail.com>
2023-02-07 13:28:35 -0300

    feat: adding file list arguments to script

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import argparse
 import csv_reader
 import simple_table_service
 
 if __name__ == '__main__':
-    df = csv_reader.read_csv("./files/trigger.csv")
-    print(df)
-    print(simple_table_service.roll_on_table(df))
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-f', '--file-list', default=[], nargs='+')
+
+    args = parser.parse_args()
+
+    print(args.file_list)
